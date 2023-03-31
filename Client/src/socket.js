6e903bfc9e9a51642d55c6791a67e1dfb6597d3f
Katie <kcossari@uwo.ca>
2023-03-31 13:25:51 -0400

    Front ad Back End

--- Client/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
-
-export const socket = io(URL);
-
-//During development, you need to enable CORS on your server:
